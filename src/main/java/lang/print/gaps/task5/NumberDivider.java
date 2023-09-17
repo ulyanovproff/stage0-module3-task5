5c3ebfca036effcc29a87f981ae7e30616278441
Pavel Ulyanov <ulyanovproff@gmail.com>
2023-09-17 23:00:05 +0500

    mission complete

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double count = (double)toBeDivided / 5;
+        System.out.println(count);
     }
 }
--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        int one = 1;
+        int two = 10;
+        int three = 100;
+
+
+
+        System.out.println(one + "" + two + "" + three);
+
     }
 }
